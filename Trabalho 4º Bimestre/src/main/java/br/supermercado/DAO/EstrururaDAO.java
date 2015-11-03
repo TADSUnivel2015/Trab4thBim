004f49b3e2782cfe83f75f9bce3eea5aebe187d0
alex tezza <tezzaalex@gmail.com>
2015-11-03 21:01:46 -0200

    Criadas as assinaturas de métodos na Interface EstruturaDAO.

--- Trabalho 4º Bimestre/src/main/java/br/supermercado/DAO/EstrururaDAO.java
+++ Trabalho 4º Bimestre/src/main/java/br/supermercado/DAO/EstrururaDAO.java
@@ -1,5 +1,19 @@
 package br.supermercado.DAO;
 
+import java.util.List;
+
 public interface EstrururaDAO {
+	
+	public void abrirConexao();
+	
+	public void gravar();
+	
+	public void atualizar();
+	
+	public void excluir();
+	
+	public List<Object> listar();
+	
+	public void fecharConexao();
 
 }
