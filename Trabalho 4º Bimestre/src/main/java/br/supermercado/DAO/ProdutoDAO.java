084e2f373354aa92abdc871afdb7d3adc9f691cf
alex tezza <tezzaalex@gmail.com>
2015-11-03 21:08:33 -0200

    Implementando os métodos da Interface EstruturaDAO em todas as Classe DAO.

--- Trabalho 4º Bimestre/src/main/java/br/supermercado/DAO/ProdutoDAO.java
+++ Trabalho 4º Bimestre/src/main/java/br/supermercado/DAO/ProdutoDAO.java
@@ -1,5 +1,43 @@
 package br.supermercado.DAO;
 
-public class ProdutoDAO {
+import java.util.List;
+
+public class ProdutoDAO implements EstrururaDAO{
+
+	@Override
+	public void abrirConexao() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void gravar() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void atualizar() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void excluir() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Object> listar() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void fecharConexao() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
