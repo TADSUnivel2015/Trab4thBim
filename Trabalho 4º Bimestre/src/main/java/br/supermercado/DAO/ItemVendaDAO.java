c226b540e2435169483f8c951f7c8eae61d521fd
alex tezza <tezzaalex@gmail.com>
2015-11-12 19:26:46 -0200

    Criando a classe ItemVendaDAO, responsável por gravar os dados da venda no banco de dados.

---
+++ Trabalho 4º Bimestre/src/main/java/br/supermercado/DAO/ItemVendaDAO.java
@@ -0,0 +1,47 @@
+package br.supermercado.DAO;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import br.supermercado.ItemVenda;
+
+public class ItemVendaDAO implements EstrururaDAO<ItemVenda> {
+
+	@Override
+	public void abrirConexao() throws SQLException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void gravar(ItemVenda itemVenda) throws SQLException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void atualizar(ItemVenda itemVenda) throws SQLException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void excluir(int id) throws SQLException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<ItemVenda> listar(String sql) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void fecharConexao() throws SQLException {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+}
