93b1bef06d15c49b26f6ba2e41463ca3f0b3ed57
alex tezza <tezzaalex@gmail.com>
2015-11-17 14:45:46 -0200

    Implementando a Classe RelatorioCliente.

--- Trabalho 4º Bimestre/src/main/java/br/supermercado/ExtendsMoldura/RelatorioCliente.java
+++ Trabalho 4º Bimestre/src/main/java/br/supermercado/ExtendsMoldura/RelatorioCliente.java
@@ -1,5 +1,26 @@
 package br.supermercado.ExtendsMoldura;
 
-public class RelatorioCliente {
+import java.awt.BorderLayout;
+import java.sql.SQLException;
+
+import javax.swing.JPanel;
+
+import br.supermercado.Moldura.MolduraAba;
+import br.supermercado.Moldura.MolduraAba;
+import br.supermercado.Telas.TelaCadastroUsuario;
+import br.supermercado.Telas.TelaRelatorioCliente;
+
+public class RelatorioCliente extends MolduraAba{
+
+	public RelatorioCliente() throws SQLException {
+		super();
+	}
+
+	@Override
+	protected void configuraMiolo() throws SQLException {
+		super.add(new TelaRelatorioCliente(), BorderLayout.CENTER);
+	}
+	
+	
 
 }
