e54bfeb6947f359a44c532d295e4c7839c3375cd
alex tezza <tezzaalex@gmail.com>
2015-11-17 20:16:40 -0200

    Criação da Enum Mes.

---
+++ Trabalho 4º Bimestre/src/main/java/br/supermercado/Enum/Mes.java
@@ -0,0 +1,18 @@
+package br.supermercado.Enum;
+
+public enum Mes {
+	
+	Janeiro,
+	Fevereiro,
+	Março,
+	Abril,
+	Maio,
+	Junho,
+	Julho,
+	Agosto,
+	Setembro,
+	Outubro, 
+	Novembro,
+	Dezembro;
+
+}
