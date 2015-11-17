d7c1e34d8db693a5f52011d48c4c1b56933f6aea
alex tezza <tezzaalex@gmail.com>
2015-11-17 20:14:31 -0200

    Criação da tela de consulta de Vendas.

--- Trabalho 4º Bimestre/src/main/java/br/supermercado/Telas/TelaRelatorioCliente.java
+++ Trabalho 4º Bimestre/src/main/java/br/supermercado/Telas/TelaRelatorioCliente.java
@@ -1,17 +1,41 @@
 package br.supermercado.Telas;
 
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
 import javax.swing.JRadioButton;
 import javax.swing.ButtonGroup;
 import javax.swing.JComboBox;
 import javax.swing.JTextField;
+
 import java.awt.Font;
+
 import javax.swing.JButton;
 
+import br.supermercado.DAO.ClienteDAO;
+import br.supermercado.Enum.Estado;
+import br.supermercado.ModelTabelas.TabelaClientes;
+
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.BorderLayout;
+import java.sql.SQLException;
+
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+
 public class TelaRelatorioCliente extends JPanel {
+	
 	private final ButtonGroup buttonGroup = new ButtonGroup();
 	private JTextField txtCidade;
+	private JComboBox cbEstado;
+	
+	private String consultaSQL;
+	private JTable tblClientes;
+	
+	private ClienteDAO clienteDAO = new ClienteDAO();
 
 	/**
 	 * Create the panel.
@@ -25,16 +49,36 @@ public class TelaRelatorioCliente extends JPanel {
 		add(lblFiltrarPor);
 		
 		JRadioButton rbCidade = new JRadioButton("Cidade");
+		rbCidade.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent arg0) {
+				
+				cbEstado.setEnabled(false);
+				
+				txtCidade.setEnabled(true);
+				
+			}
+		});
 		buttonGroup.add(rbCidade);
 		rbCidade.setBounds(130, 55, 74, 23);
 		add(rbCidade);
 		
 		JRadioButton rbEstado = new JRadioButton("Estado");
+		rbEstado.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent e) {
+				
+				txtCidade.setEnabled(false);
+				
+				cbEstado.setEnabled(true);
+				
+			}
+		});
 		buttonGroup.add(rbEstado);
 		rbEstado.setBounds(549, 55, 81, 23);
 		add(rbEstado);
 		
-		JComboBox cbEstado = new JComboBox(new Object[]{});
+		cbEstado = new JComboBox(Estado.values());
 		cbEstado.setEnabled(false);
 		cbEstado.setBounds(636, 56, 81, 20);
 		add(cbEstado);
@@ -48,8 +92,59 @@ public class TelaRelatorioCliente extends JPanel {
 		JPanel panel = new JPanel();
 		panel.setBounds(33, 108, 1254, 425);
 		add(panel);
+		panel.setLayout(new BorderLayout(0, 0));
+		
+		JScrollPane scrollPane = new JScrollPane();
+		panel.add(scrollPane, BorderLayout.CENTER);
+		
+		tblClientes = new JTable();
+		scrollPane.setViewportView(tblClientes);
 		
 		JButton button = new JButton("Pesquisar");
+		button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				if (rbCidade.isSelected() || rbEstado.isSelected()){
+					
+					if (rbCidade.isSelected()){
+						
+						consultaSQL = "select * from cliente where cidade = '" + txtCidade.getText() + "'";
+						
+					} else {
+						
+						consultaSQL = "select * from cliente where estado = '" +  cbEstado.getSelectedItem().toString() + "'";
+						
+					}
+					
+					try {
+						clienteDAO.abrirConexao();
+						
+						tblClientes.setModel(new TabelaClientes(clienteDAO.listar(consultaSQL)));
+						
+						limparCampos();
+						
+						clienteDAO.fecharConexao();
+					} catch (SQLException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					
+					
+					
+				} else {
+					JOptionPane.showMessageDialog(null, "Por gentileza, selecione um filtro de pesquisa.");
+				}
+				
+			}
+
+			private void limparCampos() {
+			
+				txtCidade.setText("");
+				
+				cbEstado.setSelectedIndex(0);
+				
+			}
+		});
 		button.setBounds(927, 51, 175, 31);
 		add(button);
 		
--- Trabalho 4º Bimestre/src/main/java/br/supermercado/Telas/TelaRelatorioVenda.java
+++ Trabalho 4º Bimestre/src/main/java/br/supermercado/Telas/TelaRelatorioVenda.java
@@ -1,14 +1,58 @@
 package br.supermercado.Telas;
 
 import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JRadioButton;
+import javax.swing.JTextField;
+import java.awt.BorderLayout;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 
 public class TelaRelatorioVenda extends JPanel {
+	private JTextField textField;
+	private JTable tblVendas;
 
 	/**
 	 * Create the panel.
 	 */
 	public TelaRelatorioVenda() {
+		setLayout(null);
+		
+		JLabel lblFiltrarPor = new JLabel("Filtrar por:");
+		lblFiltrarPor.setBounds(36, 50, 76, 14);
+		add(lblFiltrarPor);
+		
+		JRadioButton rdbtnPorDia = new JRadioButton("Por dia");
+		rdbtnPorDia.setBounds(118, 46, 76, 23);
+		add(rdbtnPorDia);
+		
+		JRadioButton rdbtnPorMs = new JRadioButton("Por m\u00EAs");
+		rdbtnPorMs.setBounds(306, 46, 81, 23);
+		add(rdbtnPorMs);
+		
+		JRadioButton rdbtnCategoriaDeProduto = new JRadioButton("Categoria de produto");
+		rdbtnCategoriaDeProduto.setBounds(548, 46, 145, 23);
+		add(rdbtnCategoriaDeProduto);
+		
+		JRadioButton rdbtnNomeCliente = new JRadioButton("Nome cliente");
+		rdbtnNomeCliente.setBounds(871, 46, 109, 23);
+		add(rdbtnNomeCliente);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(36, 93, 1051, 354);
+		add(panel);
+		panel.setLayout(new BorderLayout(0, 0));
+		
+		JScrollPane scrollPane = new JScrollPane();
+		panel.add(scrollPane, BorderLayout.CENTER);
+		
+		tblVendas = new JTable();
+		scrollPane.setViewportView(tblVendas);
+		
+		textField = new JTextField();
+		textField.setBounds(200, 46, 100, 23);
+		add(textField);
+		textField.setColumns(10);
 
 	}
-
 }
