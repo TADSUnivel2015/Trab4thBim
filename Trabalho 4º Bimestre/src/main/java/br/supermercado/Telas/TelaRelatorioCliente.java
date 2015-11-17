b77ac679ce55f57f03c6f99be0771eb1e9e040d1
alex tezza <tezzaalex@gmail.com>
2015-11-17 14:42:02 -0200

    Criação da JPanel TelaRelatorioClinte.

---
+++ Trabalho 4º Bimestre/src/main/java/br/supermercado/Telas/TelaRelatorioCliente.java
@@ -0,0 +1,14 @@
+package br.supermercado.Telas;
+
+import javax.swing.JPanel;
+
+public class TelaRelatorioCliente extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public TelaRelatorioCliente() {
+
+	}
+
+}
